327ba287e95adaad7ea22de64e142f63e27d6858
MUHAMMAD ANAS S <smuhammadanas021@gmail.com>
2026-04-18 16:16:00 +0530

    Uploaded resume analyzer backend

---
+++ resume-analyzer-backend/src/test/java/com/example/resumeanayzer/ResumeAnalyzer3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resumeanayzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzer3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
